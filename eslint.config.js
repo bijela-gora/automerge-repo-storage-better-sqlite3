c23f284964abde0b3ba1c53d1c25da8339281476
Bijela Gora <bijela_gora@protonmail.com>
2024-04-06 00:36:42 +0200

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
+	prettierConfig,
+	{
+		languageOptions: {
+			parserOptions: {
+				project: ['./tsconfig.eslint.json'],
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		rules: {
+			'@typescript-eslint/no-inferrable-types': 'off', // I don't like this
+			'@typescript-eslint/require-await': 'off', // async makes a function not to throw, but to reject
+			'@typescript-eslint/array-type': ['error', { default: 'array-simple', readonly: 'array-simple' }], // this looks reasonable for me
+		},
+	},
+)
--- src/BetterSqlite3StorageAdapter.test.ts
+++ src/BetterSqlite3StorageAdapter.test.ts
@@ -1,5 +1,5 @@
 import { describe, onTestFinished } from 'vitest'
-import { runStorageAdapterTests, SetupFn } from '@automerge/automerge-repo/dist/helpers/tests/storage-adapter-tests'
+import { runStorageAdapterTests, SetupFn } from '@automerge/automerge-repo/dist/helpers/tests/storage-adapter-tests.js'
 import DatabaseInstance from 'better-sqlite3'
 import { BetterSqlite3StorageAdapter } from './BetterSqlite3StorageAdapter.js'
 
