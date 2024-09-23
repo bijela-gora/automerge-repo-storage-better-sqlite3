597a5a96d774743d1a6a064a331c4bfbfcd1d65a
Bijela Gora <bijela_gora@protonmail.com>
2024-09-23 20:58:59 +0300

    Simplify

--- src/better-sqlite3-storage-adapter.test.ts
+++ src/better-sqlite3-storage-adapter.test.ts
@@ -1,19 +1,17 @@
-import { describe, onTestFinished } from 'vitest'
+import { onTestFinished } from 'vitest'
 import { runStorageAdapterTests, SetupFn } from '@automerge/automerge-repo/helpers/tests/storage-adapter-tests.js'
 import DatabaseInstance from 'better-sqlite3'
 import { BetterSqlite3StorageAdapter } from './better-sqlite3-storage-adapter.js'
 
 const setup: SetupFn = async () => {
 	const database = new DatabaseInstance(':memory:')
-	onTestFinished(() => {
+	const teardown = () => {
 		database.close()
-	})
+	}
 
 	const adapter = new BetterSqlite3StorageAdapter(database)
 
-	return { adapter }
+	return { adapter, teardown }
 }
 
-describe('BetterSqlite3StorageAdapter', () => {
-	runStorageAdapterTests(setup)
-})
+runStorageAdapterTests(setup, 'BetterSqlite3StorageAdapter')
